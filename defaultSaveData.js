09a0ce40e9342b637adfc31c63ddfd0e4fddc7fa
David Gundry <david@gundry.co.uk>
2020-02-10 16:36:39 +0000

    Updated default level data

--- defaultSaveData.js
+++ defaultSaveData.js
@@ -1,82 +1,107 @@
 let defaultSaveData = 
 {
+    maxAgentsPerTeam: 20,
+    maxProjectiles: 100,
     teams: [
         {
-            currentAgents: 1, // If this is wrong can throw errors?
+            currentAgents: undefined,
             movesMade: 0,
             isPlaying: true,
-            killCount: [0,0],
+            killCount: [0,0,0],
+            agents: [{
+                collideRadius: 0.5,
+                x: 8.5,
+                y: 1.5,
+                velocityX: 0,
+                velocityY: 0,
+                inUse: true,
+                team: 0,    
+                health: 10,
+                speed: 5,
+                maxHealth: 10,
+            },
+            {
+                collideRadius: 0.5,
+                x: 4,
+                y: 12,
+                velocityX: 0,
+                velocityY: 0,
+                inUse: true,
+                team: 0,    
+                health: 10,
+                speed: 5,
+                maxHealth: 10,
+            }]
         },
         {
-            currentAgents: 1,
+            currentAgents: undefined,
             movesMade: 0,
             isPlaying: true,
-            killCount: [0,0],
+            killCount: [0,0,0],
+            agents: [
+                {
+                    collideRadius: 0.5,
+                    x: 13,
+                    y: 3,
+                    velocityX: 0,
+                    velocityY: 0,
+                    inUse: true,
+                    team: 1,    
+                    health: 10,
+                    speed: 5,
+                    maxHealth: 10,
+                },
+                {
+                    collideRadius: 0.5,
+                    x: 13,
+                    y: 13,
+                    velocityX: 0,
+                    velocityY: 0,
+                    inUse: true,
+                    team: 1,    
+                    health: 10,
+                    speed: 5,
+                    maxHealth: 10,
+                }]
+        },
+        {
+            currentAgents: undefined,
+            movesMade: 0,
+            isPlaying: true,
+            killCount: [0,0,0],
+            agents: [
+                {
+                    collideRadius: 0.5,
+
+                    x: 1.5,
+                    y: 1.5,
+                    velocityX: 0,
+                    velocityY: 0,
+                    inUse: true,
+                    team: 2,    
+                    health: 10,
+                    speed: 5,
+                    maxHealth: 10,
+                }
+            ]
         }
     ],
-    map: [[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1]],
-    influence: null,
-    agents: [[
-        {
-            collideRadius: 0.5,
-            scale: 1,
-            uid: 1,
-            x: 1,
-            y: 1,
-            rotation: 0,
-            velocityX: 0,
-            velocityY: 0,
-            inUse: true,
-            team: 0,
-            //crouching: false,
-            //shieldUp: false,
-            ammoCurrent: 1,
-            health: 1,
-            speed: 1,
-            maxHealth: 1,
-            meleeDamage: 1,
-            rangedDamage: 1,
-            reloadTime: 1,
-            rateOfFire: 1,
-            ammoClipSize: 1,
-            burstFirePossible: true,
-            burstFireOn: false,
-            rotSpeed: 1,
-            rotVelocity: 1,
-            reloadWait: 0,
-            shieldUp: false
-        }
-    ],
-    [
-        {
-            collideRadius: 0.5,
-            scale: 1,
-            uid: 1,
-            x: 1,
-            y: 1,
-            rotation: 0,
-            velocityX: 0,
-            velocityY: 0,
-            inUse: true,
-            team: 0,
-            //crouching: false,
-            //shieldUp: false,
-            ammoCurrent: 1,
-            health: 1,
-            speed: 1,
-            maxHealth: 1,
-            meleeDamage: 1,
-            rangedDamage: 1,
-            reloadTime: 1,
-            rateOfFire: 1,
-            ammoClipSize: 1,
-            burstFirePossible: true,
-            burstFireOn: false,
-            rotSpeed: 1,
-            rotVelocity: 1,
-            reloadWait: 0,
-            shieldUp: false
-        }
-    ]]
+    map: [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],//0
+          [1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1],
+          [1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1],
+          [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
+          [1,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1],
+          [1,0,0,1,1,1,0,0,0,1,1,0,0,0,0,1],
+          [1,0,0,0,1,1,0,0,0,1,1,1,0,0,1,1],
+          [1,0,0,0,0,0,0,0,0,1,1,1,0,0,1,1],
+          [1,0,0,0,0,0,0,0,0,1,1,1,0,0,1,1],//8
+          [1,1,1,1,0,0,1,1,1,1,1,0,0,0,0,1],
+          [1,1,1,1,0,0,1,1,1,1,1,0,0,0,0,1],
+          [1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,1],
+          [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,1],
+          [1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1],
+          [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]],//15
 }
 export { defaultSaveData };
